376771ad8f2b4b260f573448904b5972efacd84a
hideUW <hideyukikomaki1996@keio.jp>
2018-03-08 10:17:06 -0800

    show data table in admin page

--- website/src/Admin.js
+++ website/src/Admin.js
@@ -8,9 +8,35 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 
 class Admin extends Component {
+
+    constructor(props) {
+        super(props) 
+        this.state ={
+            data : []
+        }
+    }
+
+    componentDidMount() {
+        this.dataRef = firebase.database().ref('form/');
+        let datas = []
+        let comp = this;
+        this.dataRef.on('value', (snapshot) => {
+            console.log('snapshot', snapshot.val())
+            snapshot.forEach(function (child) {
+                datas.push(child.val());
+            })
+            comp.setState({
+                data:datas 
+            })
+        })
+        console.log(datas);
+        console.log(this.state.data);
+    }
 
     handleSignOut() {
         firebase.auth().signOut().then(
@@ -20,21 +46,60 @@ class Admin extends Component {
         );
     }
 
+
     render() {
 
         return(
         <MuiThemeProvider>
             <Card style={{margin:100}}>
             <div>
+                <CardText>
+                    <MakeDataTable data={this.state.data}></MakeDataTable>
+                </CardText>
                 <RaisedButton label="Sign out" 
                               onClick={() => this.handleSignOut()} 
-                              style={{ smargin: 'auto' }} />
+                              style={{ margin: 30 }} />
             </div>
             </Card>
         </MuiThemeProvider>
         );
     }
 }
+
+class MakeDataTable extends Component {
+    render(){
+        return(
+            <div>
+                <ReactTable
+                data={this.props.data}
+                columns={[
+                {
+                    Header: "Name",
+                    accessor: 'name'
+                },
+                {
+                    Header: "Email",
+                    accessor: 'email'
+                },
+                {
+                    Header: 'Time',
+                    accessor: 'time'
+                },
+                {
+                    Header: 'Subject',
+                    accessor: 'subject'
+                },
+                {
+                    Header: 'Message',
+                    accessor: 'message'
+                }
+                ]}
+                defaultPageSize={20}
+                className="-striped -highlight"/>
+            </div>
+        );
+    }
+  }
 
 export default Admin;
 
--- website/src/Contact.js
+++ website/src/Contact.js
@@ -23,6 +23,7 @@ export class Contact extends Component {
       password: '',
       name: '',
       email: '',
+      time: '',
       subject:'',
       message:'',
       form: {},
